9074f1428f81c24021f67dc003ddbbe48ab1c1f6
Hank <Hank@DESKTOP-7MFLCB8>
2017-05-09 15:41:24 +0800

    添加项目文件。

---
+++ Client_Side/Client_Side.h
@@ -0,0 +1,32 @@
+
+// Client_Side.h : PROJECT_NAME 应用程序的主头文件
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "在包含此文件之前包含“stdafx.h”以生成 PCH 文件"
+#endif
+
+#include "resource.h"		// 主符号
+
+
+// CClient_SideApp: 
+// 有关此类的实现，请参阅 Client_Side.cpp
+//
+
+class CClient_SideApp : public CWinApp
+{
+public:
+	CClient_SideApp();
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+
+// 实现
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CClient_SideApp theApp;
---
+++ Client_Side/Header.h
@@ -0,0 +1,17 @@
+#pragma once
+//各类型
+const int MSG_LOGIN = 0x01;//由客户端发出登录请求
+const int MSG_TLOGIN = 0x02;//由服务端校对账号密码后，允许客户端登录
+const int MSG_FLOGIN = 0x03;//账号密码不正确，拒绝客户端登录
+const int MSG_SENDPERSON = 0x11;//发送单人信息
+const int MSG_SENDGROUP = 0x12;//发送群消息
+typedef struct HEADER
+{
+	int Type;
+	int ContentLength;
+	char FromUser[8];
+	char ToUser[8];
+	char GroupID[8];
+	char Account[8];
+	char Password[8];
+}Header,*LPHeader;
